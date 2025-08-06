5ddeeb57ad6f362129afbd53a024da97986c076a
N Ramesh <nagurramesh221b@gmail.com>
2025-08-06 14:14:39 +0530

    #commit

---
+++ src/components/Demo.js
@@ -0,0 +1,13 @@
+import React, { Component } from "react";
+
+class Demo extends Component {
+     render() {
+        return (
+            <div>
+                <h1>Hello World...... CSE here .... late and lazy</h1>
+            </div>
+        );
+     }
+}
+
+export default Demo;
